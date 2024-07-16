26faf9242a7d2acb50c50454c433d2d55cef9317
Gongas <goncalo.mesteves03@gmail.com>
2024-07-16 19:00:16 +0100

    fixed the attributes names when acessing the configuration

--- Transformer From Scratch/Train.py
+++ Transformer From Scratch/Train.py
@@ -15,6 +15,8 @@ from torch.utils.data import (Dataset, DataLoader, random_split)
 
 from Dataset import (BilingualDataset, causal_mask)
 
+from Model import (Transformer, Build_Transformer)
+
 from datasets import (load_dataset)
 from tokenizers import (Tokenizer)
 from tokenizers.models import (WordLevel)
@@ -61,11 +63,11 @@ def Get_Dataset(config):
     := param: config - Configuration of the Model
     """
     # Extrating dynamically the Dataset from HuggingFace
-    raw_dataset = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+    raw_dataset = load_dataset('opus_books', f'{config["source_language"]}-{config["target_language"]}', split='train')
 
     # Build the tokenizers
-    tokenizer_source = Get_or_Build_Tokenizer(config, raw_dataset, config['lang_src'])
-    tokenizer_target = Get_or_Build_Tokenizer(config, raw_dataset, config['lang_tgt'])
+    tokenizer_source = Get_or_Build_Tokenizer(config, raw_dataset, config['source_language'])
+    tokenizer_target = Get_or_Build_Tokenizer(config, raw_dataset, config['target_language'])
 
     # Split the Data (90% for Trainning and 10% for Testing / Validation)
     train_dataset_size = int(0.9 * len(raw_dataset))
@@ -73,16 +75,16 @@ def Get_Dataset(config):
     raw_train_dataset, raw_test_dataset = random_split(raw_dataset, [train_dataset_size, test_dataset_size])
 
     # Creating the respective datasets from the splitted data
-    train_dataset = BilingualDataset(raw_train_dataset, tokenizer_source, tokenizer_target, config['lang_src'], config['lang_tgt'], config['seq_len'])
-    test_dataset = BilingualDataset(raw_test_dataset, tokenizer_source, tokenizer_target, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    train_dataset = BilingualDataset(raw_train_dataset, tokenizer_source, tokenizer_target, config['source_language'], config['target_language'], config['sequence_length'])
+    test_dataset = BilingualDataset(raw_test_dataset, tokenizer_source, tokenizer_target, config['source_language'], config['target_language'], config['sequence_length'])
 
     # In order to choose the Max Sequence Length we need to find the Maximum length of the sentence in the source and target
     max_length_source = 0
     max_length_target = 0
 
     for item in raw_dataset:
-        source_ids = tokenizer_source.encode(item['translation'][config['lang_src']]).ids
-        target_ids = tokenizer_source.encode(item['translation'][config['lang_tgt']]).ids
+        source_ids = tokenizer_source.encode(item['translation'][config['source_language']]).ids
+        target_ids = tokenizer_source.encode(item['translation'][config['target_language']]).ids
         max_length_source = max(max_length_source, len(source_ids))
         max_length_target = max(max_length_target, len(target_ids))
     
@@ -97,5 +99,15 @@ def Get_Dataset(config):
     return train_dataloader, test_dataloader, tokenizer_source, tokenizer_target
 
 # Build the Model
-def Get_Model():
-    ...
+def Get_Model(config, source_vocabulary_size, target_vocabulary_size):
+    """
+    := param: config
+    := param: source_vocabulary_size
+    := param: target_vocabulary_size
+    """
+    
+    # Create a Transformer
+    model = Build_Transformer(source_vocabulary_size, target_vocabulary_size, config['sequence_length'], config['sequence_length'], config['dim_model'])
+
+    # Return the new Model
+    return model
